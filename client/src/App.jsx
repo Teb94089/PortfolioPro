dace7116d94c49c8febf2abeeb3ce952de1a8049
Teb94089 <nkulie12345@gmail.com>
2023-08-19 21:54:21 +0200

    latest changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import Home from './PortfolioContainer/Home/Home'
-
+import PortfolioContainer from './PortfolioContainer/PortfolioContainer';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
 function App() {
   return <div className="App">
-    
-    <Home/>
+    <ToastContainer />
+    <PortfolioContainer/>
     </div>
 
 }
 
-export default App
+export default App;
