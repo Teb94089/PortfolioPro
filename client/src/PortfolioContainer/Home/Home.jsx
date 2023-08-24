1bca93049613093240eb63d7351f25c30f6fcb7b
Teb94089 <nkulie12345@gmail.com>
2023-08-24 10:24:25 +0200

    Navbar changes

--- client/src/PortfolioContainer/Home/Home.jsx
+++ client/src/PortfolioContainer/Home/Home.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import Header from "./Header/Header"
+import  Nav from "./Nav/Nav";
 import Profile from  "./Profile/Profile";
 import Footer from "./Footer/Footer"
 import "./Home.css";
@@ -8,7 +9,7 @@ import "./Home.css";
 export default function Home(props) {
   return (
     <div className="home-container" id={props.id || ""}>
-      <Header />
+      <Nav />
       <Profile />
       <Footer />
     </div>
---
+++ client/src/PortfolioContainer/Home/Nav/Nav.jsx
@@ -0,0 +1,39 @@
+import './Nav.css';
+
+import React from "react"
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import logo from "../../../assets/header/logo.png";
+
+export default function Nav() {
+  return (
+    <div className="nav-section">
+      <div className="nav-container">
+        <a href="#Home">
+          <div className="nav-logo">
+            <span>
+              <img src={logo} alt="image not responding" />
+            </span>
+          </div>
+        </a>
+        <FontAwesomeIcon className="hamburger-menu " icon={faBars} />
+        <ul>
+          <li>
+            <a href="#Home">Home</a>
+          </li>
+          <li>
+            <a href="#AboutMe">About me</a>
+          </li>
+          <li>
+            <a href="#Projects">Projects</a>
+          </li>
+          <li>
+            <a href="#ContactMe">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
